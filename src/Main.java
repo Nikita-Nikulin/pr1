c154e7bc6fb059117e20dfb277f298c86355c58d
ivc6-10 <nik.nik99@mail.ru>
2020-09-03 11:08:29 +0300

    инициализация прокта 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        float[] arr1 = {1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 };
+        float [] arr2=new float[10];
+        System.out.println("задание 1");
+        for (int i=0;i<10;i++)
+        {
+            arr2 [i] = 1/arr1[i];
+            System.out.println(arr2[i]);
+        }
+        System.out.println("задание 2");
+        int a=0;
+        int b=100;
+        for (int i=0;i<10;i++)
+        {
+            arr1[i]= a+(int) (Math)
+        }
+
+    }
+}
